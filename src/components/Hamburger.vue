bd1b3eb56efcc4f97e1411779cfc4fa97a1eac47
Krisztian Toth <ktoth2@emarsys.com>
2019-05-18 11:13:53 +0200

    Add static hamburger landing page

---
+++ src/components/Hamburger.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>Bun</div>
+    <div>+</div>
+    <div>Bun</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Hamburger"
+  }
+</script>
+
+<style scoped>
+
+</style>
